f87c73b9fd77feaf87b122d06383c7682f5b9eae
Chris Collins <chris@collins.id.au>
2009-12-08 23:02:40 +1100

    Removed unused :secret key from forgery protection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '534f346ba6d9ab4315f9e88eef868ebf'
+  protect_from_forgery
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
